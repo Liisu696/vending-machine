1a69a0bd63201af4a698d53bdfae80cd0df63345
offthebench <saxena.akshay319@gmail.com>
2021-02-21 14:16:26 +0200

    feat: added directory structure

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import manager.VendingMachine;
+import service.DatabaseService;
+import service.DatabaseServiceImpl;
+
+public class Application {
+
+    public static void main(String[] args) {
+    }
+}
